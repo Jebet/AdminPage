f69ea004844d79cf2e93d2c9582fcab6e7a1df29
eclay jebet <eclay.jebet@shujaaz.com>
2019-06-25 17:15:21 +0300

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Admin from "./adminDashboard";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ui container">
+        <Admin />
+      </div>
+    );
+  }
+}
+
+export default App;
