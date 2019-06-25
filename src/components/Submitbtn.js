68ce2feb4aedf56c88ab21a198ae9a78e1186c82
eclay jebet <eclay.jebet@shujaaz.com>
2019-06-25 17:15:43 +0300

    Create Submitbtn.js

---
+++ src/components/Submitbtn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+const Submitbutton = () => (
+  <div>
+    <Button primary>Send</Button>
+  </div>
+);
+
+export default Submitbutton;
